55c3b14a4dec7a98f3e7bf6dc7bb61d4f9c142d7
Daniel Beilfuss <d.beilfuss@icloud.com>
2023-10-07 13:36:19 -0600

    completed part 2

--- about.js
+++ about.js
@@ -1,14 +1,34 @@
 console.log("hello world");
 
+/////// Variables  //////
+let form = document.querySelector("#contact");
+let images = document.querySelectorAll(".image");
 
-
+//////  Functions  //////
 function handleSubmit(evt) {
-	evt.preventDefault();
-	
-	console.log('form submit');
+  evt.preventDefault();
+
+  alert("Form Submitted!");
 }
 
+function randomItem(array) {
+  const randomDigit = Math.floor(Math.random() * array.length);
+  console.log(randomDigit);
+  return array[randomDigit];
+}
 
-let form = document.querySelector('#contact');
+function complimentUser(evt) {
+  const complimentsNodeList = document.querySelectorAll(".compliment");
+  const complimentsArray = Array.from(complimentsNodeList).map((li) =>
+    li.textContent.trim()
+  );
+  const compliment = randomItem(complimentsArray);
+  alert(compliment);
+}
 
-form.addEventListener('submit', handleSubmit);
+//////  Event Listeners  //////
+form.addEventListener("submit", handleSubmit);
+console.log(images);
+for (let image of images) {
+  image.addEventListener("mouseover", complimentUser);
+}
